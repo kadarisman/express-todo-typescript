dbe62114b3a41cb9fbed89dd9210e7a735748d94
kadarisman <yuzarqadariesman@gmail.com>
2023-05-01 07:17:33 +0700

    test deploy lagi

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,8 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database":process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": "mysql2"
   },
   "test": {
     "username": "root",
